f6cc8cb56314f07822d52f2364087ee1065c343c
Andrey Salnikov <a.salnikov.home@gmail.com>
2018-03-31 12:58:20 +0300

    abstr+interface

--- abstractclass/Program.cs
+++ abstractclass/Program.cs
@@ -6,10 +6,143 @@ using System.Threading.Tasks;
 
 namespace abstractclass
 {
+    //public abstract class DbSet<T> where T : class
+    //{
+    //    public bool Create(T entity) 
+    //    {
+    //        Console.WriteLine("was createt");
+    //        return true;
+    //    }
+    //    public T Read (T entity)
+    //    {
+    //        Console.WriteLine("was read");
+    //        return null;
+    //    }
+    //    public bool Update(T entity)
+    //    {
+    //        Console.WriteLine("was updated");
+    //        return true;
+    //    }
+    //    public bool Delete (T entity)
+    //    {
+    //        Console.WriteLine("was deleted");
+    //        return true;
+    //    }
+    //}
+    public abstract class BaseRepositry<T> where T : class
+    {
+        public bool Create(T entity)
+        {
+            Console.WriteLine(entity.GetType());
+            Console.WriteLine("was created");
+            return true;
+        }
+        public T Read(T entity)
+        {
+            Console.WriteLine("was read");
+            return null;
+        }
+        public bool Update(T entity)
+        {
+            Console.WriteLine("was updated");
+            return true;
+        }
+        public bool Delete(T entity)
+        {
+            Console.WriteLine("was deleted");
+            return true;
+        }
+    }
+
+    class FoodRepository : BaseRepositry<Food>
+    {
+
+    }
+
+    class Food
+    {
+        public decimal Price { get; set; }
+        public string Name{ get; set; }
+        public List<Ingridient> Ingredients { get; set; }
+
+    }
+    class Ingridient
+    {
+        public string Name{ get; set; }
+        public double Protein { get; set; }
+        public double Fats { get; set; }
+    }
+
+    class Drinks
+    {
+        public string Name { get; set; }
+        public double Price{ get; set; }
+    }
+
+    class DrinksRepository : BaseRepositry<Drinks>
+    {
+
+    }
+
+    public class Human<T>
+    {
+        public T MyProperty { get; set; }
+
+    }
+
+
+
+    abstract class Oven
+    {
+        public abstract void Heat();
+
+        public void Print()
+        {
+            Console.WriteLine("Yo");
+        }
+    }
+
+    class GasOven : Oven
+    {
+        public int MyProperty { get; set; }
+
+        public override void Heat()
+        {
+            Console.WriteLine("Gas");
+        }
+    }
+    class ElectricOven : Oven 
+        {
+        public override void Heat()
+        {
+            Console.WriteLine("Electric");
+        }
+    }
     class Program
     {
+
         static void Main(string[] args)
         {
+            Oven oven = new GasOven();
+            oven.Heat();
+
+
+            Food food = new Food();
+            food.Name = "Salat";
+            food.Price = 153;
+            food.Ingredients = new List<Ingridient>();
+            food.Ingredients.Add(new Ingridient { Fats = 15.3, Name = "Truf", Protein = 15.6 });
+
+            Food food1 = food;
+            Console.WriteLine($"{food.GetHashCode()}, {food1.GetHashCode()}");
+
+            BaseRepositry<Food> foodRepository = new FoodRepository();
+            foodRepository.Create(food);
+
+            Drinks drinks = new Drinks { Name = "Beer", Price = 10};
+            BaseRepositry<Drinks> drinksRepository = new DrinksRepository();
+            drinksRepository.Create(drinks);
+
         }
     }
 }
---
+++ interface/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @interface
+{
+    interface IWriteToFile
+    {
+
+        void Write();
+
+    }
+
+
+    interface ICountOfAngles
+    {
+        int GetAngles();
+    }
+    interface ICoords
+    {
+        int x { get; set; }
+        int y { get; set; }
+    }
+    abstract class Figure
+    {
+        public string Name { get; set; }
+        public double Perimetr{ get; set; }
+        public double Area { get; set; }
+        public abstract double GetPer();
+        public abstract double GetArea();
+    }
+    class Triangle : Figure, ICountOfAngles, ICoords
+    {
+        public double Storona{ get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public int GetAngles()
+        {
+            return 3;
+        }
+
+        public override double GetArea()
+        {
+            Area = Math.Sqrt(3) / 4 * Math.Pow(Storona, 2);
+                return Area;
+        }
+
+        public override double GetPer()
+        {
+            Perimetr = Storona * 3;
+            return Perimetr;
+        }
+    }
+
+    class Circle : Figure, ICoords
+    {
+        public double Radius { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public override double GetArea()
+        {
+            Area = Math.PI * Math.Pow ( Radius,2);
+            return Area;
+        }
+        public override double GetPer()
+        {
+            Perimetr = 2 * Math.PI * Radius;
+            return Perimetr;
+                
+        }
+    }
+
+    class Sqare : Figure, ICoords, ICountOfAngles
+    {
+        public int Storona { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public int GetAngles()
+        {
+            return 4;
+        }
+        public override double GetArea()
+        {
+            Area = Math.Pow(Storona, 2);
+            return Area;
+        }
+        public override double GetPer()
+        {
+            Perimetr = Storona*4;
+            return Perimetr;
+        }
+    }
+
+    class StrWriter : IWriteToFile
+    {
+        public void Write()
+        {
+             // srteam writer
+        }
+    }
+
+    class Program
+    {
+        static int GetAngles(ICountOfAngles countAngles)
+        {
+            return countAngles.GetAngles();
+        }
+        static void PrintCoords(ICoords coords)
+        {
+            Console.WriteLine($"{coords.x} - {coords.y}");
+        }
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            circle.x = 15;
+            circle.y = 1;
+            PrintCoords(circle);
+
+            IWriteToFile writeToFile = 
+
+        }
+    }
+}
